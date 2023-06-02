2411da4c145c1b3789eebe318310b5be53c54b8f
suraj898958 <surajkumar875jsr@gmail.com>
2023-06-02 16:35:25 +0530

    Initial commit

---
+++ src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../img/logo (1).png'
+
+const Header = () => {
+  return (
+    <header className='center'>
+      <img src={logo} alt='' />s
+    </header>
+  )
+}
+
+export default Header
